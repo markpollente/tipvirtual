e30c1ed401ad0d381673053e061cbb815eff6577
Michael Pollente <pollentem@gmail.com>
2023-11-17 22:35:48 +0800

    Add progress bar

--- src/components/Tour/Tour.tsx
+++ src/components/Tour/Tour.tsx
@@ -1,5 +1,7 @@
 import { MpSdk, setupSdk } from "@matterport/sdk";
 import { useEffect, useRef, useState } from "react";
+import { Progress } from 'react-sweet-progress';
+import "react-sweet-progress/lib/style.css";
 
 import "./Tour.css";
 
@@ -8,6 +10,7 @@ function Tour() {
   const [horizontal, setHorizontal] = useState(45);
   const [vertical, setVertical] = useState(15);
   const [favorites, setFavorites] = useState<any[]>([]);
+  const [visited, setVisited] = useState(new Set<number>());
 
   const container = useRef<HTMLDivElement>(null);
 
@@ -26,63 +29,53 @@ function Tour() {
   }, []);
 
   useEffect(() => {
-    let buttonDisplayed: boolean;
-    initTags().then(() => {
-      sdk?.Tag.data.subscribe({
-        onAdded(index, item, collection) {
-          console.log('Tag added to the collection', index, item, collection);
-        },
-        onRemoved(index, item, collection) {
-          console.log('Tag removed from the collection', index, item, collection);
-        },
-        onUpdated(index, item, collection) {
-          console.log('Tag updated in place in the collection', index, item, collection);
-        },
-        onCollectionUpdated(collection) {
-          console.log('The full collection of Tags looks like', collection);
+    sdk?.Tag.data.subscribe({
+      onAdded(index, item, collection) {
+        console.log('Tag added to the collection', index, item, collection);
+      },
+      onRemoved(index, item, collection) {
+        console.log('Tag removed from the collection', index, item, collection);
+      },
+      onUpdated(index, item, collection) {
+        console.log('Tag updated in place in the collection', index, item, collection);
+      },
+      onCollectionUpdated(collection) {
+        console.log('The full collection of Tags looks like', collection);
 
-          let newFavorites = [];
-          for (const [_, value] of collection) {
-            newFavorites.push({ ...value})
-          }
-          setFavorites(newFavorites);
+        let newFavorites = [];
+        for (const [_, value] of collection) {
+          newFavorites.push(value);
         }
-      });
+        setFavorites([...newFavorites]);
+      }
     });
   }, [sdk]);
-
-  const initTags = async () =>  {
-    await sdk?.Mattertag.add([{
-      label: 'Main Building',
-      description: "",
-      anchorPosition: {x: -78.8348218075196, y: 1.224310426550836, z: -43.62401061071125},
-      stemVector: { // make the Tag stick straight up and make it 0.30 meters (~1 foot) tall
-        x: 0,
-        y: 0.30,
-        z: 0,
-      },
-      color: { // blue disc
-        r: 0.0,
-        g: 0.0,
-        b: 1.0,
-      },
-    }
-      ]);
-  }
-
+  sdk?.Camera.pose.subscribe(pose => {
+    let position = pose.position;
+    favorites.forEach(favorite => {
+      const favoritePosition = favorite.anchorPosition;
+      if (!visited.has(favorite.id) && Math.abs(favoritePosition.x - position.x) <= 10 && Math.abs(favoritePosition.z - position.z) <= 3) {
+        visited.add(favorite.id);
+        setVisited(new Set(visited));
+      }
+    });
+  });
   const rotate = () => {
     sdk?.Camera.rotate(horizontal, vertical);
   };
 
   const goToFavorite = (sid: any) => {
     sdk?.Mattertag.navigateToTag(sid, sdk?.Mattertag.Transition.FLY);
+    visited.add(sid);
+    setVisited(new Set(visited));
   }
 
   return (
     <>
       <div className="container" ref={container} ></div>
-
-
+      {visited.size}
+      {favorites.length}
+      <Progress percent={visited.size / favorites.length * 100} />
       <div className="grid gap-2.5 my-5 mx-auto max-w-[150px] justify-center">
       {/*<ol>
           {favorites.map(fav => <li key={fav.id}>
